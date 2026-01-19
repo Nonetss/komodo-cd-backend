ca8de84f62d806521973ae4660ae2e68796c912f
nonete <antonio.moreno@sjd.es>
2026-01-19 12:19:37 +0000

    feat: add role and banned columns to auth_user table and update corresponding migration and snapshot files

--- src/db/models/users.table.ts
+++ src/db/models/users.table.ts
@@ -6,6 +6,8 @@ export const authUser = pgTable("auth_user", {
   email: text("email").notNull().unique(),
   emailVerified: boolean("email_verified").notNull().default(false),
   image: text("image"),
+  role: text("role"),
+  banned: boolean("banned"),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });

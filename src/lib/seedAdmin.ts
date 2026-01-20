509c51cfb465d65bb3381bea51d89b4a48de9fbc
Nonete <amorenolopezbarajas@pm.me>
2026-01-20 12:59:04 +0100

    feat: update .env.example and seedAdmin function to use new admin user configuration

--- src/lib/seedAdmin.ts
+++ src/lib/seedAdmin.ts
@@ -3,9 +3,9 @@ import { auth } from "@/db/auth";
 export const seedAdmin = async () => {
   const { user, token } = await auth.api.signUpEmail({
     body: {
-      email: process.env.SEED_USER_EMAIL!,
-      name: process.env.SEED_USER_NAME!,
-      password: process.env.SEED_USER_PASSWORD!,
+      email: process.env.SEED_ADMIN_EMAIL!,
+      name: process.env.SEED_ADMIN_NAME!,
+      password: process.env.SEED_ADMIN_PASSWORD!,
     },
   });
 

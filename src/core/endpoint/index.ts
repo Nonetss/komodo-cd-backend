a25bc5ca289c9d23040cbec34485d4681b9dcf6e
Nonete <amorenolopezbarajas@pm.me>
2026-02-17 19:24:17 +0100

    refactor: improve error handling and logging in StandardEndpoint class for better debugging

---
+++ src/core/endpoint/index.ts
@@ -0,0 +1,6 @@
+import { PageEndpoint } from "@/core/endpoint/type/page.endpoint";
+import { StandardEndpoint } from "@/core/endpoint/type/standard.endpoint";
+import { EmptyEndpoint } from "@/core/endpoint/type/empty.endpoint";
+import { ErrorEndpoint } from "@/core/endpoint/type/error.endpoint";
+
+export { PageEndpoint, StandardEndpoint, EmptyEndpoint, ErrorEndpoint };
